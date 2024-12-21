171ce1d9dd96fb7d7bf3f046c0497985ff576972
Bakhtiyar Bobokulov <b.bobokulov@outlook.com>
2024-12-22 02:09:25 +0300

    feat: sorting types

---
+++ org.sorting/src/main/java/org.sorting/SelectionSort.java
@@ -0,0 +1,20 @@
+package org.sorting;
+
+public class SelectionSort {
+
+    public int[] selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        return arr;
+    }
+}
